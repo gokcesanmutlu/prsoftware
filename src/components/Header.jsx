5972e9d9ea88167832666585f66213ba5842cbf3
GokceSan <gokcesan1995@gmail.com>
2024-03-11 11:56:17 +0300

    m

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -196,7 +196,7 @@ const Header = () => {
       ) : (
         ""
       )}
-
+<Link to={"/about"}>click </Link>
       {/* BÜYÜK EKRANDA RESOURCES */}
       {isResourcesOpen ? (
         // en dıştaki div bir katman olarak oluşturulu bu katmana tıklayınca resources kapanıyor
@@ -204,7 +204,7 @@ const Header = () => {
           <div className="w-full h-auto menu-breakmax:hidden fixed bg-[#fff] z-[80] top-[72px] right-0 ">
             <div
               className={`flex gap-12 m-auto items-center max-w-[1070px] h-[156px]`}
-            > <Link to="/about">click </Link>
+            > <Link to={"/about"}>click </Link>
               <Link to="/about">
                 <div className="flex gap-2">
                   <div className="left">
