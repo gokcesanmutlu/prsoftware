44caf4db4280b85b79a8405fd799fc42a656e773
GokceSan <gokcesan1995@gmail.com>
2024-03-11 10:26:48 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,28 +6,28 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
 
-    <footer className="xs:h-[328px] flex justify-between w-full bg-[#fff] py-[48px] px-[80px] menu-breakmax:py-[24px] menu-breakmax:px-[40px]">
+    <footer className="flex justify-between w-full bg-[#fff] py-[48px] px-[80px] menu-breakmax:py-[24px] menu-breakmax:px-[40px]">
       <Link to={"/"}>
         <img src="/logo.png" alt="Logo" />
       </Link>
 
-      <ul className="flex flex-wrap px-12 gap-6 text-[#344054] text-xs font-medium">
-        <li>
+      <ul className="flex flex-wrap px-12 bg:px-0 gap-6 text-[#344054] text-xs font-medium">
+        <li className="bg:pr-12 ">
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="bg:pr-12 ">
           <Link to="/about">About Us</Link>
         </li>
-        <li>
+        <li className="bg:pr-12 ">
           <Link to="/press">Press</Link>
-        </li>
-        <li>
+        </li >
+        <li className="bg:pr-16">
           <Link to="/career">Career</Link>
         </li>
-        <li>
+        <li className="bg:pr-12 ">
           <Link to="/help">Help</Link>
         </li>
-        <li>
+        <li className="bg:pr-12 ">
           <Link to="/privacy">Privacy</Link>
         </li>
       </ul>
